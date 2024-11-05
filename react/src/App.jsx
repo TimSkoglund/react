af94c0e2365da68d9e75128d9a198fb1ff40cee6
Tim Skoglund <timskoglund@hotmail.com>
2024-11-05 18:53:05 +0100

    ground styling and config added

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,4 +1,12 @@
-import './App.css'
+import './assets/css/main.css'
+import './assets/css/appfeatures.css'
+import './assets/css/button.css'
+import './assets/css/clients.css' 
+import './assets/css/contact-js.css'
+import './assets/css/contact.css'
+import './assets/css/email.css'
+import './assets/css/featuresmoney.css'
+import './assets/css/hero.css'
 
 function App() {
 
